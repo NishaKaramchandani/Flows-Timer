61cf46cc22feaa3fd31b402d4be931a4b4471790
Nisha Karamchandani <nisha.d.karamchandani@gmail.com>
2023-08-12 19:28:25 -0700

    hilt app class

---
+++ app/src/main/java/com/example/baseproject/di/BaseApplication.kt
@@ -0,0 +1,40 @@
+package com.example.baseproject.di
+
+import android.app.Application
+import com.example.baseproject.network.ApiService
+import com.example.baseproject.network.Repository
+import com.example.baseproject.network.RepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@HiltAndroidApp
+class BaseApplication: Application()
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(ApiService.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRepository(apiService: ApiService): Repository = RepositoryImpl(apiService)
+
+}
